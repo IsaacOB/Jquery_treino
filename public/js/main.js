bf108a77711977c646f8496dcb829d5e0e777956
Isaac <isaac.benites@hotmail.com>
2020-08-07 22:56:05 -0300

    Um pouco mais sobre eventos no JQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,17 @@
 var frase = $(".frase").text();
 var numeroDePalavras = frase.split(" ").length;
-
 var tamanhoDaFrase = $("#tamanhoDaFrase").text(numeroDePalavras);
+
+var campoDigitacao = $(".campoDigitacao");
+
+campoDigitacao.on("input", function(){
+
+    var conteudo = campoDigitacao.val();
+
+    var quantidadePalavras = conteudo.split(/\S+/).length -1;
+    $("#contadorPalavras").text(quantidadePalavras);
+
+    var quantidadeCaracteres = conteudo.length;
+    $("#contadorCaracteres").text(quantidadeCaracteres);
+
+});
