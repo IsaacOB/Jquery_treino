9341438373255328a45765466938ee7aee005000
Isaac <isaac.benites@hotmail.com>
2020-08-12 21:17:48 -0300

    Dinamismo na tabela e botão remover na linha de cada usuário

--- public/js/main.js
+++ public/js/main.js
@@ -89,14 +89,33 @@ function insereNovoValorNaTabela(){
     var numPalavras = $("#contadorPalavras").text();
     var usuario = "Eu :3";
 
-    var linha = "<tr>" + 
-                   "<td>" + usuario + "</td>" + 
-                   "<td>" + numPalavras + "</td>" + 
-                "</tr>"
+    var linha = novaLinhaNoPlacar(usuario, numPalavras);
+    linha.find(".botaoRemover").click(removeUsuarioDaTabela);
 
     corpoTabela.prepend(linha);                
 }
 
+function novaLinhaNoPlacar(usuario, numPalavras){
+
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaNumeroDePalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>")
+    var link = $("<a>").addClass("botaoRemover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario).append(colunaNumeroDePalavras).append(colunaRemover);
+
+    return linha;
+
+}
+
+function removeUsuarioDaTabela(){
+        event.preventDefault();
+        $(this).parent().parent().remove();
+}
 
 function finalizaESalvaDadosDoJogo(){
 
